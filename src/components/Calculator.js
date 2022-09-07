5ea93089ff02dc66cd9399703b242b96971976df
tinayetsina <tinayetsina@outlook.com>
2022-09-07 17:37:53 +0800

    update - calculator UI completed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,8 +2,45 @@ import React from 'react';
 import '../styles/Calculator.css';
 
 const Calculator = () => (
-  <div>
-    AEIOU
+  <div className="calculator-container">
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="4">0</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>AC</td>
+          <td>+/-</td>
+          <td>%</td>
+          <td className="right-column">÷</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+          <td className="right-column">x</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+          <td className="right-column">-</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td className="right-column">+</td>
+        </tr>
+        <tr>
+          <td colSpan="2">0</td>
+          <td>.</td>
+          <td className="right-column">=</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 );
 
