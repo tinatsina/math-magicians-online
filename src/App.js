0974553d016ebb093ee601de8c0154d2c5fbd509
tinayetsina <tinayetsina@outlook.com>
2022-09-07 22:32:03 +0800

    patch resolved review comments

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,47 +1,56 @@
 import React from 'react';
 import '../styles/Calculator.css';
 
-const Calculator = () => (
-  <div className="calculator-container">
-    <table>
-      <thead>
-        <tr>
-          <th colSpan="4">0</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>AC</td>
-          <td>+/-</td>
-          <td>%</td>
-          <td className="right-column">÷</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td className="right-column">x</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-          <td className="right-column">-</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td className="right-column">+</td>
-        </tr>
-        <tr>
-          <td colSpan="2">0</td>
-          <td>.</td>
-          <td className="right-column">=</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div className="calculator-container">
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="4">0</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>AC</td>
+              <td>+/-</td>
+              <td>%</td>
+              <td className="right-column">÷</td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>8</td>
+              <td>9</td>
+              <td className="right-column">x</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>5</td>
+              <td>6</td>
+              <td className="right-column">-</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td className="right-column">+</td>
+            </tr>
+            <tr>
+              <td colSpan="2">0</td>
+              <td>.</td>
+              <td className="right-column">=</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default Calculator;
