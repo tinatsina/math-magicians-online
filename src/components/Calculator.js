d63c2b8f0b3fed8c55d20b6604e4a5d46a1eff35
tinayetsina <tinayetsina@outlook.com>
2022-09-08 15:41:57 +0800

    update - onclick handler completed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -23,33 +23,33 @@ class Calculator extends React.Component {
           </thead>
           <tbody>
             <tr>
-              <td><button type="button">AC</button></td>
-              <td><button type="button">+/-</button></td>
-              <td><button type="button">%</button></td>
-              <td><button type="button" className="right-column">÷</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>AC</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>+/-</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>%</button></td>
+              <td><button type="button" onClick={this.onClickHandler} className="right-column">÷</button></td>
             </tr>
             <tr>
-              <td><button type="button">7</button></td>
-              <td><button type="button">8/-</button></td>
-              <td><button type="button">9</button></td>
-              <td><button type="button" className="right-column">x</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>7</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>8</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>9</button></td>
+              <td><button type="button" onClick={this.onClickHandler} className="right-column">x</button></td>
             </tr>
             <tr>
-              <td><button type="button">4</button></td>
-              <td><button type="button">5</button></td>
-              <td><button type="button">6</button></td>
-              <td><button type="button" className="right-column">-</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>4</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>5</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>6</button></td>
+              <td><button type="button" onClick={this.onClickHandler} className="right-column">-</button></td>
             </tr>
             <tr>
-              <td><button type="button">1</button></td>
-              <td><button type="button">2</button></td>
-              <td><button type="button">4</button></td>
-              <td><button type="button" className="right-column">+</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>1</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>2</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>4</button></td>
+              <td><button type="button" onClick={this.onClickHandler} className="right-column">+</button></td>
             </tr>
             <tr>
-              <td colSpan="2"><button type="button">0</button></td>
-              <td><button type="button">.</button></td>
-              <td><button type="button" className="right-column">=</button></td>
+              <td colSpan="2"><button type="button" onClick={this.onClickHandler}>0</button></td>
+              <td><button type="button" onClick={this.onClickHandler}>.</button></td>
+              <td><button type="button" onClick={this.onClickHandler} className="right-column">=</button></td>
             </tr>
           </tbody>
         </table>
